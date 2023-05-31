0dc0de563bd9194085537b16dcd21e6db0e3d60a
Luanaap <l.andrade2711@gmail.com>
2023-05-30 21:12:08 -0300

    Aula 3 Usando Data Binding e submit

--- 1_Instalacao_vue/src/app.js
+++ 1_Instalacao_vue/src/app.js
@@ -1,8 +1,18 @@
 const MyNameApp = {
     data() {
         return {
-            name: "Luana", 
+            name: "", 
             age: 25, 
+            input_name: "",
+        }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault();
+
+            console.log(this.input_name);
+
+            this.name = this.input_name;
         }
     }
 }
